d0d16aba16cf69752a08e9dd7cae35107964a269
Jonatas Miler <jonatasmiler@gmail.com>
2022-04-12 09:35:58 -0300

    Limpando Codigo da user rota

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const userController = require( '../controllers/users' );
 const Auth = require( '../middlewares/auth' );
 
-//router.get( '/', userController.index );
-//router.get( '/:id', userController.view );
-//router.post( '/login', userController.Login );
+
 router.get( '/lista', Auth.isAuthorized, userController.Lista );
 
 module.exports = router;
